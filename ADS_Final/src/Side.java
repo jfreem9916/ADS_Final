7ef4ced486a1a8ce2964ba83ca00880939890001
B) <jfreem9916@inst.hcpss.org>
2017-05-11 08:09:35 -0400

    sure

--- ADS_Final/src/Side.java
+++ ADS_Final/src/Side.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class Side extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4687343863014431902L;
 	private ArrayList<MemoryCard> displayOfCards;
 	public Side(){
 		this.setLayout(null);
 		this.setBackground(Color.black);
+		displayOfCards = new ArrayList<MemoryCard>();
+	}
+	public Side(Color c){
+		this.setLayout(null);
+		this.setBackground(c);
 		displayOfCards = new ArrayList<MemoryCard>();
 	}
 	public void addCard(MemoryCard m){
